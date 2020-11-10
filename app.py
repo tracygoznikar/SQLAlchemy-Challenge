be7adfea695ff060265c35e97fbb03cfd258191d
TracyGoznikar <tracygoznikar@gmail.com>
2020-11-10 15:36:09 -0600

    adds app.routes with instructions

--- app.py
+++ app.py
@@ -20,15 +20,30 @@ def index():
     )
 
 
-
 @app.route("/api/v1.0/precipitation")
+#def date_prcp():
+ # convert query results to a dictionary using date as the key and prcp as the value
+ #return JSON representation of dictionary
+
+  #  return jsonify
+@app.route("/api/v1.0/stations")
+#def stations():
+ # return JSON lost of stations from dataset
+
+  #  return jsonify
+@app.route("/api/v1.0/tobs")
+#def tobs():
+ # query the dates and temperature observations of the most active station for the last year
+ #return JSON list of TOBS for the previous year
+
+  #  return jsonify
+  @app.route("/api/v1.0/<start>")
 #def justice_league():
- #   """Return the justice league data as json"""
+ # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+ #When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+ #When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
 
-  #  return jsonify(justice_league_members)
-
-
-
+  #  return 
 
 if __name__ == "__main__":
     app.run(debug=True)
