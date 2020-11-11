ece110aa7996236fd4f2c4aaf0d23d437669e598
TracyGoznikar <tracygoznikar@gmail.com>
2020-11-10 19:36:49 -0600

    adds query code

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+conn = engine.connect()
+Base = automap_base()
 
 app = Flask(__name__)
-
 
 #################################################
 # Flask Routes
@@ -24,9 +31,9 @@ def index():
 def date_prcp():
  # convert query results to a dictionary using date as the key and prcp as the value
  session = Session(engine)
- results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>='2016-08-23')
+ prcp = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>='2016-08-23')
  session.close()
- prcp = list(np.ravel(results))
+ prcp = list(np.ravel(prcp))
  #return JSON representation of dictionary
  return jsonify(prcp)
 @app.route("/api/v1.0/stations")
@@ -35,20 +42,20 @@ def stations():
  session = Session(engine)
  station_name = session.query(Station.idd, Station.station, Stations.name).all()
  session.close()
+ station_name = list(np.ravel(station_name))
   #  return jsonify
-@app.route("/api/v1.0/tobs")
+ return jsonify(station_name)
+#@app.route("/api/v1.0/tobs")
 #def tobs():
  # query the dates and temperature observations of the most active station for the last year
  #return JSON list of TOBS for the previous year
 
   #  return jsonify
-@app.route("/api/v1.0/<start>")
+#@app.route("/api/v1.0/<start>")
 #def justice_league():
  # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
  #When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
  #When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
-
-  #  return 
 
 if __name__ == "__main__":
     app.run(debug=True)
